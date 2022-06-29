9aa8fa90ca1af1f56153dd4ded939fd1695aa33c
Kaio Marx <kaiomarxdev@gmail.com>
2022-06-29 15:29:52 -0300

    feat: implement the avatar component

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,10 +1,11 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { Avatar } from '../Avatar';
 import styles from './comment.module.css';
 
 export function Comment() {
   return (
     <div className={styles.comment} >
-      <img src="http://github.com/kaiomarxdev.png" />
+      <Avatar src="http://github.com/kaiomarxdev.png" />
 
       <div className={styles.commentBox}>
         <div className={styles.commentContent}>
