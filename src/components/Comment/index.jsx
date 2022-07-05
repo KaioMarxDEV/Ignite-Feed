05defc4074fcfc85e10c4844a9a91a827e943ea2
Kaio Marx <kaiomarxdev@gmail.com>
2022-07-05 14:41:35 -0300

    feat: like count function

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -1,10 +1,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
 import { Avatar } from "../Avatar";
 import styles from "./comment.module.css";
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleIncreaseLikeCount() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -28,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleIncreaseLikeCount}>
             <ThumbsUp size={16} />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
